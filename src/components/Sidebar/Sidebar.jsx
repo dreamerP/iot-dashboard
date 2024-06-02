bc19be4ea1e30a58910fbad1f1ad6ea88b2ec125
dreamerP <patriciacastelo99@gmail.com>
2024-06-02 12:41:46 -0400

    fix: Sidebar
    -Changed some colors

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -56,7 +56,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
       <div
         className={`${
           open ? 'w-60' : 'w-fit'
-        } hidden sm:block relative h-screen duration-300 bg-white border-r border-gray-200 dark:border-gray-600 p-5 dark:bg-slate-800`}
+        } hidden sm:block relative h-screen duration-300 bg-white border-r border-gray-200 p-5 `}
       >
         <Link to="/">
           <div className={`flex ${open && 'gap-x-1'} items-center`}>
@@ -77,10 +77,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
           {Menus.map((menu, index) => (
             <Link to={menu.path} key={index}>
               <li
-                className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200
                         ${menu.gap ? 'mt-9' : 'mt-2'} ${
-                          location.pathname === menu.path &&
-                          'bg-gray-200 dark:bg-gray-700'
+                          location.pathname === menu.path && 'bg-gray-200 '
                         }`}
               >
                 <span className="text-2xl">{menu.src}</span>
@@ -102,7 +101,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
         <div
           className={`${
             mobileMenu ? 'flex' : 'hidden'
-          } absolute z-50 flex-col items-center self-end py-8 mt-16 space-y-6 font-bold sm:w-auto left-6 right-6 dark:text-white  bg-gray-50 dark:bg-slate-800 drop-shadow md rounded-xl`}
+          } absolute z-50 flex-col items-center self-end py-8 mt-16 space-y-6 font-bold sm:w-auto left-6 right-6 dark:text-white  bg-gray-50 drop-shadow md rounded-xl`}
         >
           {Menus.map((menu, index) => (
             <Link
@@ -112,9 +111,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
             >
               <span
                 className={` ${
-                  location.pathname === menu.path &&
-                  'bg-gray-200 dark:bg-gray-700'
-                } p-2 rounded-xl hover:bg-gray-200 dark:hover:bg-gray-700`}
+                  location.pathname === menu.path && 'bg-gray-200 '
+                } p-2 rounded-xl hover:bg-gray-200 `}
               >
                 {menu.title}
               </span>
