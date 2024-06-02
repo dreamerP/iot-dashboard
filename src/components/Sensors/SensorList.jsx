854b14247c21172ca0626f01ac4aecc897bb196b
dreamerP <patriciacastelo99@gmail.com>
2024-06-02 17:08:52 -0400

    feat: Sensor List
    -Isolated main logic for sensor management in this component.

--- src/components/Sensors/SensorList.jsx
+++ src/components/Sensors/SensorList.jsx
@@ -1,50 +1,99 @@
-import React, { useMemo } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import ActionButtons from '@/components/Shared/ActionButtons/ActionButtons';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import React, { useState, useEffect } from 'react';
+import SensorTable from '@/components/Sensors/SensorTable';
+import SensorForm from '@/components/Sensors/SensorForm';
+import sensorService from '@/services/sensorService';
+import ConfirmationModal from '@/components/Shared/ConfirmationForm/ConfirmationForm';
 
-const SensorTable = ({ sensors, onEdit, onDelete }) => {
-  const columnDefs = useMemo(
-    () => [
-      { headerName: 'Type', field: 'type',  },
-      { headerName: 'Driver', field: 'driver' },
-      {
-        headerName: 'Status',
-        field: 'status',
-        cellRenderer: ({ value }) => (value ? 'Active' : 'Inactive'),
-      },
-      { headerName: 'Value', field: 'value' },
-      {
-        headerName: 'Actions',
-        field: 'actions',
-        toolPanelClass: ['tp-silver'],
-        cellRenderer: (params) => (
-          <ActionButtons
-            onEdit={() => onEdit(params.data)}
-            onDelete={() => onDelete(params.data)}
-          />
-        ),
-        cellStyle: { textAlign: 'center' },
-        flex: 1,
-      },
-    ],
-    [onEdit, onDelete]
-  );
+const SensorList = () => {
+  const [sensors, setSensors] = useState([]);
+  const [editingSensor, setEditingSensor] = useState(null);
+  const [showForm, setShowForm] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedSensor, setSelectedSensor] = useState(null);
+
+  useEffect(() => {
+    const fetchSensors = async () => {
+      const sensors = await sensorService.getAllSensors();
+      setSensors(sensors);
+    };
+    fetchSensors();
+  }, []);
+
+  const handleEdit = (sensor) => {
+    setEditingSensor(sensor);
+    setShowForm(true);
+  };
+
+  const handleDelete = async (sensor) => {
+    setSelectedSensor(sensor);
+    setShowModal(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (selectedSensor) {
+      await sensorService.deleteSensor(selectedSensor.id);
+      setSensors(sensors.filter((s) => s.id !== selectedSensor.id));
+    }
+    setShowModal(false);
+    setSelectedSensor(null);
+  };
+
+  const handleCancelDelete = () => {
+    setShowModal(false);
+    setSelectedSensor(null);
+  };
+
+  const handleCloseForm = () => {
+    setEditingSensor(null);
+    setShowForm(false);
+  };
 
   return (
-    <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
-      <AgGridReact
-        rowData={sensors}
-        columnDefs={columnDefs}
-        rowSelection={'multiple'}
-        checkboxSelection={true}
-        pagination={true}
-        paginationPageSize={20}
-        defaultColDef={{ sortable: true, filter: true }}
-      />
+    <div className="grid grid-cols-12 p-4 gap-6">
+      <div className="col-span-12 flex justify-between p-3">
+        <div className="flex text-2xl font-sans text-slate-600 p-3">
+          <h3>Sensor List</h3>
+        </div>
+        <div className=" flex justify-end p-3">
+          <button
+            className="bg-blue-500 text-white text-md p-1 w-32 rounded-md mr-4 flex items-center"
+            onClick={() => setShowForm(true)}
+          >
+            <img src="plus_ico_white.svg" alt="Edit" className="w-6 h-5" />
+            New Sensor
+          </button>
+          <button
+            className="bg-red-500 text-white text-md p-1 w-32 rounded-md flex items-center"
+            onClick={() => setShowForm(true)}
+          >
+            <img src="delete_ico_white.svg" alt="Edit" className="w-6 h-5" />
+            Delete (0)
+          </button>
+        </div>
+      </div>
+      <div className="col-span-12">
+        <SensorTable
+          sensors={sensors}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+        {showForm && (
+          <div className="modal">
+            <SensorForm sensor={editingSensor} onClose={handleCloseForm} />
+          </div>
+        )}
+        {showModal && (
+          <ConfirmationModal
+            open={showModal}
+            title="Confirm Deletion"
+            message={`Are you sure you want to delete the sensor: ${selectedSensor?.type}?`}
+            onConfirm={handleConfirmDelete}
+            onCancel={handleCancelDelete}
+          />
+        )}
+      </div>
     </div>
   );
 };
 
-export default SensorTable;
+export default SensorList;
