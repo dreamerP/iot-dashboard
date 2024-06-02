0f5d80179256f33ef6e85be407017544e3927695
dreamerP <patriciacastelo99@gmail.com>
2024-06-02 19:20:21 -0400

    fix: Sensor Table
    -Allow multiple selection using checkbox for datatable.

--- src/components/Sensors/SensorTable.jsx
+++ src/components/Sensors/SensorTable.jsx
@@ -1,13 +1,14 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useRef, useCallback } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import ActionButtons from '@/components/Shared/ActionButtons/ActionButtons';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
-const SensorTable = ({ sensors, onEdit, onDelete }) => {
+const SensorTable = ({ sensors, onEdit, onDelete, onSelectionChanged }) => {
+  const gridRef = useRef();
   const columnDefs = useMemo(
     () => [
-      { headerName: 'Type', field: 'type' },
+      { headerName: 'Type', field: 'type', checkboxSelection: true },
       { headerName: 'Driver', field: 'driver' },
       {
         headerName: 'Status',
@@ -34,12 +35,16 @@ const SensorTable = ({ sensors, onEdit, onDelete }) => {
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
       <AgGridReact
+        ref={gridRef}
         rowData={sensors}
         columnDefs={columnDefs}
         rowSelection={'multiple'}
         checkboxSelection={true}
         pagination={true}
         paginationPageSize={20}
+        onSelectionChanged={() =>
+          onSelectionChanged(gridRef.current.api.getSelectedRows())
+        }
         defaultColDef={{ sortable: true, filter: true }}
       />
     </div>
