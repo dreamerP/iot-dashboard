3a5ae3eba4d8bfcda19020f36627d97164243f75
dreamerP <patriciacastelo99@gmail.com>
2024-06-03 17:48:28 -0400

    fix: authService Frontend -Added function to storage token and to logout.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,19 +1,43 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "http://localhost:3000/api";
 
 const authService = {
-  login: async (email, password) => {
-    const response = await axios.post(`${API_URL}/auth/login`, { email, password });
+  login: async (username = "admin", password = "admin") => {
+    const response = await axios.post(`${API_URL}/auth/login`, {
+      username,
+      password,
+    });
+    const { token } = response.data;
+    if (token) {
+      localStorage.setItem('token', token);
+    }
     return response.data;
   },
 
   logout: async () => {
+    localStorage.removeItem('token');
     await axios.post(`${API_URL}/auth/logout`);
   },
 
   refreshTokens: async (refreshToken) => {
-    const response = await axios.post(`${API_URL}/auth/refresh`, { refreshToken });
+    const response = await axios.post(`${API_URL}/auth/refresh`, {
+      refreshToken,
+    });
+    const { token } = response.data;
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+    return response.data;
+  },
+  checkAuth: async () => {
+    const token = localStorage.getItem('token');
+    if (!token) throw new Error('No token found');
+    const response = await axios.get(`${API_URL}/auth/checkAuth`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return response.data;
   },
 };
