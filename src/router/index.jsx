3437ed58fa9b4433ee37f7a3d3d5c7d27af89a44
dreamerP <patriciacastelo99@gmail.com>
2024-06-03 17:47:27 -0400

    feat: Routes Index -Added Routes and configured default behaivor.

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,16 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from '@/context/AuthContext.jsx';
-import PrivateRoute from '@/router/PrivateRoute.jsx';
-import Login from '@/components/Login/LoginForm';
-import Dashboard from '@/pages/Dashboard';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider } from "@/context/AuthContext.jsx";
+import PrivateRoute from "@/router/PrivateRoute.jsx";
+import Login from "@/components/Login/LoginForm";
+import Dashboard from "@/pages/Dashboard";
 
 const AppRouter = () => (
   <AuthProvider>
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-        <Route path="/sensors" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route
+          exact
+          path="/dashboard"
+          element={<PrivateRoute element={<Dashboard />} />}
+        />
+        <Route
+          path="/sensors"
+          element={<PrivateRoute element={<Dashboard />} />}
+        />
       </Routes>
     </Router>
   </AuthProvider>
