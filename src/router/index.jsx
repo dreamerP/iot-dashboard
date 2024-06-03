a40e234e4d19d2c93a70aef57f4da0db0785949d
dreamerP <patriciacastelo99@gmail.com>
2024-06-03 02:26:01 -0400

    fix: Routes -Added authorization to routed.

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from '@/context/AuthContext.jsx';
+import PrivateRoute from '@/router/PrivateRoute.jsx';
 import Login from '@/components/Login/LoginForm';
 import Dashboard from '@/pages/Dashboard';
 
 const AppRouter = () => (
-  <Router>
-    <Routes>
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/dashboard" element={<Dashboard/>} />
-      <Route path="/sensors" element={<Login />} />
-    </Routes>
-  </Router>
+  <AuthProvider>
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/sensors" element={<PrivateRoute element={<Dashboard />} />} />
+      </Routes>
+    </Router>
+  </AuthProvider>
 );
 
 export default AppRouter;
