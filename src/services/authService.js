a21ee432060ac2153bb176ffb2bc3a5e47e58289
dreamerP <patriciacastelo99@gmail.com>
2024-06-03 12:45:15 -0400

    feat: Auth Service Frontend
    -Service to allow an user to use login, logout and refreshtoken.

---
+++ src/services/authService.js
@@ -0,0 +1,22 @@
+// authService.js
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api';
+
+const authService = {
+  login: async (email, password) => {
+    const response = await axios.post(`${API_URL}/auth/login`, { email, password });
+    return response.data;
+  },
+
+  logout: async () => {
+    await axios.post(`${API_URL}/auth/logout`);
+  },
+
+  refreshTokens: async (refreshToken) => {
+    const response = await axios.post(`${API_URL}/auth/refresh`, { refreshToken });
+    return response.data;
+  },
+};
+
+export default authService;
