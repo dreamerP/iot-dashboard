ec71619d46b01608f2cf5a6d7abda9be1f4d1afa
dreamerP <patriciacastelo99@gmail.com>
2024-06-03 00:48:40 -0400

    fix: configurations -Relocated configurations related to testing to <rootDir>/config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,12 @@
 export default {
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>/config/setupTests.js'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
   moduleFileExtensions: ['js', 'jsx'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|sass|scss)$': '<rootDir>/styleMock.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/config/styleMock.js',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
