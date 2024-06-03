475792d32e862f6dbdb0f7e85c2c494b54d19f44
dreamerP <patriciacastelo99@gmail.com>
2024-06-03 00:07:28 -0400

    fix: Action Buttons -Fixed a className prop and some styles.

--- src/components/Shared/ActionButtons/ActionButtons.jsx
+++ src/components/Shared/ActionButtons/ActionButtons.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const ActionButtons = ({ onEdit, onDelete }) => {
   return (
     <div className="flex  py-1">
-      <button className=" mr-4" onClick={onEdit}>
-      <img src="edit_ico.svg" alt="Edit" className="w-6 h-5" />
+      <button className="mr-4" onClick={onEdit}>
+        <img src="edit_ico.svg" alt="Edit" className="w-6 h-5" />
       </button>
       <button onClick={onDelete}>
-      <img src="delete_ico_red.svg" alt="Edit" className="w-6 h-5" />
+        <img src="delete_ico_red.svg" alt="Delete" className="w-6 h-5" />
       </button>
     </div>
   );
