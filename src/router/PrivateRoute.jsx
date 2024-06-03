96286f95b5ec3ebef21a220de5c46fd1edf479e7
dreamerP <patriciacastelo99@gmail.com>
2024-06-03 17:41:07 -0400

    fix:Private Route -Code formatting.

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '@/context/AuthContext.jsx';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext.jsx";
 
 const PrivateRoute = ({ element }) => {
   const { isAuthenticated } = useAuth();
-
   return isAuthenticated ? element : <Navigate to="/login" />;
 };
 
